9bddb6a37d49ffae5cb873fcef527e7a90df2578
WANG Guoqin <wangguoqin1001@gmail.com>
2014-02-06 10:43:24 -0500

    Rails initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aquarius::Application.config.secret_key_base = '30b87e669a412b9e023dfc56946ffa7072ca8086992b25a7bcb745df259fc77fedf1f0fa4b4ac442564c81c3e4d60487ebb74d8cba4318fbe8d3c81f1544a789'
